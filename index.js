c00c2fa2784ef315017ef889693a9841511d256e
mohamedsabith <sabithmohd101@gmail.com>
2023-01-02 10:44:54 +0530

    story auto view

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import { IgApiClient } from "instagram-private-api";
+import fs from "fs";
 import chalk from "chalk";
-import lodash from 'lodash'
-
-import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import lodash from "lodash";
+import cron from "node-cron";
+import * as dotenv from "dotenv";
 dotenv.config();
 
 const ig = new IgApiClient();
@@ -12,19 +13,31 @@ ig.state.generateDevice(process.env.IG_USERNAME);
   await ig.simulate.preLoginFlow();
   ig.account
     .login(process.env.IG_USERNAME, process.env.IG_PASSWORD)
-    .then((res) => {
-        fetchFollowers(ig.state, 'mohamedsabithmp').then((followersId) => {
-            fetchReel(followersId).then((res) => {
-              res.items.forEach(e => {
-                ig.story.seen(e).then((response) =>{
-                  res.userNames.forEach(e => console.log(chalk.blue("\x1b[32m%s\x1b[0m", "USER =====>", e.username , "MEDIA_ID =====> " , e.media_id )))
-               }).catch((err) => {
-                  console.log(err);
-               })
+    .then(() => {
+      cron.schedule("* * * * *", () => {
+        fetchReel().then((res) => {
+          res.items.forEach((e) => {
+            ig.story
+              .seen(e)
+              .then(() => {
+                res.userNames.forEach((e) =>
+                  console.log(
+                    chalk.blue(
+                      "\x1b[32m%s\x1b[0m",
+                      "USER =====>",
+                      e.username,
+                      "MEDIA_ID =====> ",
+                      e.media_id
+                    )
+                  )
+                );
               })
-            })
-        })
-       
+              .catch((err) => {
+                console.log(err);
+              });
+          });
+        });
+      });
     })
     .catch((err) => {
       console.log(chalk.red(err.message));
@@ -32,33 +45,26 @@ ig.state.generateDevice(process.env.IG_USERNAME);
     });
 })();
 
-const fetchFollowers = async (state, user) => {
-  try {
-    const pk = []
-    ig.state = state;
-    const userId = await ig.user.getIdByUsername(user);
-    const followersFeed = await ig.feed.accountFollowers(userId);
-    const followerItem = await followersFeed.items();
-    followerItem.forEach(e => {
-        pk.push(e.pk)
-    })
-    return pk;
-  } catch (error) {
-    console.log(error);
-  }
-};
+const fetchReel = async () => {
+  const idArray = [];
+  const userNames = [];
+  const data = await ig.feed.reelsTray("pull_to_refresh");
 
-const fetchReel = async (userId) => {
-  const idArray = []
-  const userNames = []
-   const data = await ig.feed.reelsTray();
-   const reel = await data.items()
-  reel.forEach(e => {
-    e.media_ids.forEach(d => {
-      userNames.push({username: e.user.username,media_id:d})
-      idArray.push({id:d + '_' + e.id ,taken_at: e.latest_reel_media,user: {pk: e.id}})
-    })
-  })
-  var items = lodash.chunk(idArray, 5)
-   return {items, userNames};
+  const reel = await data.items();
+  reel.forEach((e) => {
+    e.media_ids.forEach((d) => {
+      userNames.push({ username: e.user.username, media_id: d });
+      idArray.push({
+        id: d + "_" + e.id,
+        taken_at: e.latest_reel_media,
+        user: { pk: e.id },
+      });
+    });
+  });
+  var writeStream = fs.createWriteStream(`${process.env.IG_USERNAME}.log`);
+  userNames.forEach((username) => {
+    writeStream.write(username.media_id + ",");
+  });
+  var items = lodash.chunk(idArray, 5);
+  return { items, userNames };
 };
